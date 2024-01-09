d3842693f77afdfc647d5edc607ee05f42f748f9
Dennis Herrera <dennishumher25@hotmail.com>
2024-01-09 12:45:33 -0600

    added getTrademarks options

--- src/trademarks/trademarks.controller.ts
+++ src/trademarks/trademarks.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Query,
+} from '@nestjs/common';
+import { TrademarksService } from './trademarks.service';
 
 @Controller('trademarks')
-export class TrademarksController {}
+export class TrademarksController {
+  constructor(private trademarksService: TrademarksService) {}
+
+  @Get()
+  getTrademarks(@Query('name') name: string) {
+    return this.trademarksService.getTrademarks(name);
+  }
+
+  @Get(':id')
+  async getTrademarkById(@Param('id') rawId: number) {
+    const id = Number(rawId);
+    if (isNaN(id)) {
+      throw new BadRequestException('Id must be a number');
+    }
+
+    const trademark = await this.trademarksService.getTrademarkById(id);
+    if (!trademark) throw new NotFoundException('Trademark does not exist');
+    return trademark;
+  }
+}
--- src/trademarks/trademarks.service.ts
+++ src/trademarks/trademarks.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Trademark } from '@prisma/client';
 
 @Injectable()
-export class TrademarksService {}
+export class TrademarksService {
+  constructor(private prisma: PrismaService) {}
+
+  getTrademarks(name: string): Promise<Trademark[]> {
+    if (name) {
+      return this.prisma.trademark.findMany({
+        where: { name },
+      });
+    } else {
+      return this.prisma.trademark.findMany();
+    }
+  }
+
+  getTrademarkById(id: number) {
+    return this.prisma.trademark.findUnique({
+      where: { id },
+    });
+  }
+}
