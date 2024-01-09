f5cbf55b9eaf3d67ed755ddec53869e2992244f5
Dennis Herrera <dennishumher25@hotmail.com>
2024-01-08 21:34:36 -0600

    added get and update categories

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { Category } from '@prisma/client';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private categoriesService: CategoriesService) {}
+
+  @Get()
+  getCategories() {
+    return this.categoriesService.getCategories();
+  }
+
+  @Post()
+  createCategory(@Body() data: Category) {
+    return this.categoriesService.createCategory(data);
+  }
+}
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Category } from '@prisma/client';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(private prisma: PrismaService) {}
+
+  getCategories() {
+    return this.prisma.category.findMany();
+  }
+
+  createCategory(data: Category): Promise<Category> {
+    return this.prisma.category.create({
+      data,
+    });
+  }
+}
