69f51285abde4544aa630a13265a1a1317363faa
Dennis Herrera <dennishumher25@hotmail.com>
2024-01-08 23:11:49 -0600

    added update and delete category options

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { Category } from '@prisma/client';
+import { updateCategoryDTO } from './dto/category.dto';
 
 @Controller('categories')
 export class CategoriesController {
@@ -14,5 +24,36 @@ export class CategoriesController {
   @Post()
   createCategory(@Body() data: Category) {
     return this.categoriesService.createCategory(data);
+  }
+
+  @Patch(':id')
+  async updateCategory(
+    @Param('id') rawId: string,
+    @Body() data: updateCategoryDTO,
+  ) {
+    const id = Number(rawId);
+    if (isNaN(id)) {
+      throw new BadRequestException('No category found.');
+    }
+
+    try {
+      return await this.categoriesService.updateCategory(id, data);
+    } catch (error) {
+      throw new BadRequestException('Fields were not filled correctly.');
+    }
+  }
+
+  @Delete(':id')
+  async deleteCategory(@Param('id') rawId: string) {
+    const id = Number(rawId);
+    if (isNaN(id)) {
+      throw new BadRequestException('No category found.');
+    }
+
+    try {
+      return await this.categoriesService.deleteCategory(id);
+    } catch (error) {
+      throw new BadRequestException('Fields were not filled correctly.');
+    }
   }
 }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { Category } from '@prisma/client';
+import { updateCategoryDTO } from './dto/category.dto';
 
 @Injectable()
 export class CategoriesService {
@@ -13,6 +14,19 @@ export class CategoriesService {
   createCategory(data: Category): Promise<Category> {
     return this.prisma.category.create({
       data,
+    });
+  }
+
+  updateCategory(id: number, data: updateCategoryDTO): Promise<Category> {
+    return this.prisma.category.update({
+      where: { id },
+      data,
+    });
+  }
+
+  deleteCategory(id: number): Promise<Category> {
+    return this.prisma.category.delete({
+      where: { id },
     });
   }
 }
