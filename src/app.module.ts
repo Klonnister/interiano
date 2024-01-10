70f999617f0351b17f7c891fe03c6a42210d93cc
Dennis Herrera <dennishumher25@hotmail.com>
2024-01-10 12:05:50 -0600

    added public directory, to serve static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 import { TrademarksModule } from './trademarks/trademarks.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ProductsModule, CategoriesModule, TrademarksModule],
+  imports: [
+    ProductsModule,
+    CategoriesModule,
+    TrademarksModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
