a50ff1128cec0bc6453591a1d99d06bcc64fe746
Dennis Herrera <dennishumher25@hotmail.com>
2024-01-27 14:23:46 -0600

    created authorization module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { CategoriesModule } from './categories/categories.module';
 import { TrademarksModule } from './trademarks/trademarks.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -13,6 +15,8 @@ import { join } from 'path';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
