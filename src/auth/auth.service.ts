006487ff3348494095c0c1f74e8c6f6d8dad66b6
Dennis Herrera <dennishumher25@hotmail.com>
2024-02-11 15:13:52 -0600

    added jwt package and login logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,15 +2,19 @@ import {
   BadRequestException,
   Injectable,
   NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
-import { RegisterDto } from 'src/auth/dto/auth.dto';
+import { JwtService } from '@nestjs/jwt';
+import { RegisterDto, LoginDto } from './dto/auth.dto';
 import * as bcrypt from 'bcrypt';
-import { LoginDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async register({ username, password }: RegisterDto) {
     const user = await this.usersService.findUser(username);
@@ -25,10 +29,21 @@ export class AuthService {
     });
   }
 
-  async login(userData: LoginDto) {
-    const user = await this.usersService.findUser(userData.username);
+  async login({ username, password }: LoginDto) {
+    const user = await this.usersService.findUser(username);
     if (!user) throw new NotFoundException('Este usuario no existe.');
 
-    user;
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword)
+      throw new UnauthorizedException('La contraseña es incorrecta');
+
+    const payload = { username: user.username, role: user.role };
+    const token = await this.jwtService.signAsync(payload);
+
+    return {
+      token,
+      username: user.username,
+      role: user.role,
+    };
   }
 }
