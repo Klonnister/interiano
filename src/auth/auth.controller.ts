4f5965a5f571bba8ffef424ee6346e67093b093c
Dennis Herrera <dennishumher25@hotmail.com>
2024-02-11 09:48:56 -0600

    added authorization controller and service logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { registerDto } from './dto/register.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signUp(@Body() userInfo: registerDto) {
+    return this.authService.register(userInfo);
+  }
+
+  @Post('login')
+  async logIn(@Body('username') username: string) {
+    username;
+    return this.authService.login();
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { registerDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  register(userInfo: registerDto) {
+    return this.usersService.createUser(userInfo);
+  }
+
+  login() {
+    return 'login';
+  }
+}
