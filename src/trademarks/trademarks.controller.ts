5ab98697c5c82c053721d2819fbc5a9645bb1d06
Dennis Herrera <dennishumher25@hotmail.com>
2024-04-04 16:20:53 -0600

    chore: added image delete on trademark update or delete

--- src/trademarks/trademarks.controller.ts
+++ src/trademarks/trademarks.controller.ts
@@ -19,7 +19,7 @@ import { ExistentTrademarkPipe } from './pipes/existent-trademark.pipe';
 import { DeletableTrademarkPipe } from './pipes/deletable-trademark.pipe';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import { existsSync } from 'fs';
+import { existsSync, unlinkSync } from 'fs';
 import { mkdir } from 'fs/promises';
 import { ValidTrademarkPipe } from './pipes/valid-trademark.pipe';
 
@@ -84,17 +84,63 @@ export class TrademarksController {
     @UploadedFile() images: Express.Multer.File,
   ): Promise<Trademark> {
     if (images) {
-      data.image = images.filename;
+      data.image = `/trademark-imgs/${images.filename}`;
     }
 
     return this.trademarksService.createTrademark(data);
   }
 
   @Patch(':id')
+  @UseInterceptors(
+    FileInterceptor('images', {
+      limits: {
+        fileSize: 1000000,
+      },
+      storage: diskStorage({
+        destination: async (req, file, cb) => {
+          // Validate if public directory exists
+          if (!existsSync('./public/trademark-imgs')) {
+            await mkdir('./public/trademark-imgs');
+          }
+
+          cb(null, './public/trademark-imgs');
+        },
+        filename: (req, file, cb) => {
+          // create image name
+          const time = new Date().getTime();
+          const ext = file.originalname.slice(
+            file.originalname.lastIndexOf('.'),
+          );
+          const imageName = time + ext;
+          const acceptedExts = ['.jpeg', '.jpg', '.png', '.webp', '.svg'];
+
+          if (!acceptedExts.includes(ext)) {
+            return cb(
+              new UnsupportedMediaTypeException(
+                'El archivo debe ser de tipo imagen.',
+              ),
+              null,
+            );
+          }
+
+          return cb(null, imageName);
+        },
+      }),
+    }),
+  )
   async updateTrademark(
     @Param('id', ValidTrademarkPipe) id: number,
+    @UploadedFile() images: Express.Multer.File,
     @Body() data: TrademarkDTO,
   ): Promise<Trademark> {
+    if (images) {
+      if (data.image) {
+        unlinkSync(`./public${data.image}`);
+      }
+
+      data.image = `/trademark-imgs/${images.filename}`;
+    }
+
     return await this.trademarksService.updateTrademark(id, data);
   }
 
@@ -102,6 +148,11 @@ export class TrademarksController {
   async deleteTrademark(
     @Param('id', DeletableTrademarkPipe) id: number,
   ): Promise<Trademark> {
+    const trademark = await this.getTrademarkById(id);
+    if (trademark.image) {
+      unlinkSync(`./public${trademark.image}`);
+    }
+
     return await this.trademarksService.deleteTrademark(id);
   }
 }
