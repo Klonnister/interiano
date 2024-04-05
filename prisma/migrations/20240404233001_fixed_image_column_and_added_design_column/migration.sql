1d760de160b206909615e231f1c47102653236d4
Dennis Herrera <dennishumher25@hotmail.com>
2024-04-05 17:48:12 -0600

    chore: changed products image upload logic

---
+++ prisma/migrations/20240404233001_fixed_image_column_and_added_design_column/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `images` on the `products` table. All the data in the column will be lost.
+  - Added the required column `design` to the `products` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `image` to the `products` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `products` DROP COLUMN `images`,
+    ADD COLUMN `design` INTEGER NOT NULL,
+    ADD COLUMN `image` VARCHAR(191) NOT NULL;
---
+++ prisma/migrations/20240405234059_changed_image_column_to_not_nullable/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Made the column `image` on table `products` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE `products` MODIFY `image` VARCHAR(191) NOT NULL;
