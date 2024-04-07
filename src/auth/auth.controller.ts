b3ece39b0ca2caf9216b7ee139e39c1418418ad5
Dennis Herrera <dennishumher25@hotmail.com>
2024-04-07 13:15:48 -0600

    chore: deleted image endpoint from auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,68 +1,11 @@
-import {
-  BadRequestException,
-  Body,
-  Controller,
-  Post,
-  UnsupportedMediaTypeException,
-  UploadedFile,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto, RegisterDto } from 'src/auth/dto/auth.dto';
 import { Public } from './decorators/public.decorator';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import { unlinkSync } from 'fs';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-
-  @Post('images')
-  @UseInterceptors(
-    FileInterceptor('images', {
-      limits: {
-        fileSize: 1000000,
-      },
-      storage: diskStorage({
-        destination: './public/profile/',
-        filename: (req, file, cb) => {
-          // create image name
-          const time = new Date().getTime();
-          const ext = file.originalname.slice(
-            file.originalname.lastIndexOf('.'),
-          );
-          const imageName = time + ext;
-          const acceptedExts = ['.jpeg', '.jpg', '.png', '.webp', '.svg'];
-
-          if (!acceptedExts.includes(ext)) {
-            return cb(
-              new UnsupportedMediaTypeException(
-                'El archivo debe ser de tipo imagen.',
-              ),
-              null,
-            );
-          }
-
-          return cb(null, imageName);
-        },
-      }),
-    }),
-  )
-  @Public()
-  saveImage(
-    @UploadedFile() images: Express.Multer.File,
-    @Body('previousImage') previousImage: string,
-  ) {
-    if (!images)
-      throw new BadRequestException('Seleccione una imagen para guardar.');
-
-    if (previousImage) {
-      unlinkSync(`./public${previousImage}`);
-    }
-
-    return `/profile/${images.filename}`;
-  }
 
   @Post('signup')
   @Public()
