dc63802e526f83d83849f7cec32f0a1b1053a045
Dennis Herrera <dennishumher25@hotmail.com>
2024-05-22 13:22:04 -0600

    fix: changed orderBy column from price to applied_price

--- src/products/pipes/product-order-by.pipe.ts
+++ src/products/pipes/product-order-by.pipe.ts
@@ -3,7 +3,7 @@ import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 @Injectable()
 export class ProductOrderByPipe implements PipeTransform {
   transform(order: string) {
-    if (!order) return { price: 'asc' };
+    if (!order) return { applied_price: 'asc' };
 
     const orderArr: string[] = order.split(',');
     if (orderArr.length <= 1)
@@ -22,7 +22,7 @@ export class ProductOrderByPipe implements PipeTransform {
       case 'category':
         return { category: { name: orderType } };
       case 'price':
-        return { price: orderType };
+        return { applied_price: orderType };
       default:
         throw new BadRequestException('Error al ordenar los productos.');
     }
