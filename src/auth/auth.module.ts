c0af74d201510c2bbcd49ebdbb240ec24e5acff9
Dennis Herrera <dennishumher25@hotmail.com>
2024-05-30 11:33:39 -0600

    chore: added more time to token expiration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { AuthGuard } from './guards/auth.guard';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '4h' },
+      signOptions: { expiresIn: '24h' },
     }),
   ],
   controllers: [AuthController],
